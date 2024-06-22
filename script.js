2f89fee294df6dbdba0145e7972ec82174a09dfe
Aditya <adit.sharma283@gmail.com>
2024-06-22 12:14:50 +0530

    testing this feature

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Retrieve todo from local storage or initialize an empty array
+
 let todo = JSON.parse(localStorage.getItem("todo")) || [];
 const todoInput = document.getElementById("todoInput");
 const todoList = document.getElementById("todoList");
@@ -6,12 +6,12 @@ const todoCount = document.getElementById("todoCount");
 const addButton = document.querySelector(".btn");
 const deleteButton = document.getElementById("deleteButton");
 
-// Initialize
+
 document.addEventListener("DOMContentLoaded", function () {
   addButton.addEventListener("click", addTask);
   todoInput.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
-      event.preventDefault(); // Prevents default Enter key behavior
+      event.preventDefault(); 
       addTask();
     }
   });
